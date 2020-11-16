a46fa5dcfbfb65052aa412477757c7a23eb1dd30
Roberto Candales <robertocandales@Robertos-MacBook-Pro.local>
2020-11-16 16:11:39 -0300

    error fix menu v1.2

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 // next.config.js
 const withImages = require('next-images');
 module.exports = withImages();
+
+module.exports = {
+  reactStrictMode: true,
+};
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
